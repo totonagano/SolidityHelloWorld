05db147c4f6efdb48db2fee81b8dba8fa10901b6
Tony DeMillard <ademillard@gmail.com>
2018-02-15 10:26:08 -0700

    Added plumbing

--- FoodSafe/app/javascripts/app.js
+++ FoodSafe/app/javascripts/app.js
@@ -3,6 +3,9 @@ import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract'
 var accounts;
 var account;
+var foodSafeABI;
+var foodSafeContract;
+var foodSafeCode;
 
 window.App = {
   start: function() {
@@ -21,6 +24,12 @@ window.App = {
       accounts = accs;
       account = accounts[0];
       web3.eth.defaultAccount = account;
+      var foodSafeSource = 'pragma solidity ^0.4.6; contract FoodSafe {    struct Location {        string Name;        uint LocationId;        uint PreviousLocationId;        uint Timestamp;        string Secret;    }    mapping(uint => Location) Trail;    uint8 TrailCount = 0;    function AddNewLocation(uint LocationId, string Name, string Secret) {        Location memory newLocation;        newLocation.Name = Name;        newLocation.LocationId = LocationId;        newLocation.Secret = Secret;        newLocation.Timestamp = now;        if (TrailCount != 0) {            newLocation.PreviousLocationId = Trail[TrailCount].LocationId;        }        Trail[TrailCount] = newLocation;        TrailCount++;    }    function GetTrailCount() returns (uint8) {        return TrailCount;    }        function GetLocation(uint8 TrailNo) returns (string,uint,uint,uint,string) {        return(Trail[TrailNo].Name, Trail[TrailNo].LocationId, Trail[TrailNo].PreviousLocationId, Trail[TrailNo].Timestamp, Trail[TrailNo].Secret);    }}';
+      web3.eth.compile.solidity(foodSafeSource, function(error, foodSafeCompiled) {
+        foodSafeABI = foodSafeCompiled['<stdin>:FoodSafe'].info.abiDefinition;
+        foodSafeContract = web3.eth.contract(foodSafeABI);
+        foodSafeCode = foodSafeCompiled['<stdin>:FoodSafe'].code;
+      })
     });
   },
 };
