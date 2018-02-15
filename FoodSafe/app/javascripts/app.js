cd51ea58745d522675bc072f27520f0f03867994
Tony DeMillard <ademillard@gmail.com>
2018-02-15 11:43:56 -0700

    Compiling the contract does not working. 'Invalid response' coming from json rpc

--- FoodSafe/app/javascripts/app.js
+++ FoodSafe/app/javascripts/app.js
@@ -24,12 +24,16 @@ window.App = {
       accounts = accs;
       account = accounts[0];
       web3.eth.defaultAccount = account;
+      console.log(account);
       var foodSafeSource = 'pragma solidity ^0.4.6; contract FoodSafe {    struct Location {        string Name;        uint LocationId;        uint PreviousLocationId;        uint Timestamp;        string Secret;    }    mapping(uint => Location) Trail;    uint8 TrailCount = 0;    function AddNewLocation(uint LocationId, string Name, string Secret) {        Location memory newLocation;        newLocation.Name = Name;        newLocation.LocationId = LocationId;        newLocation.Secret = Secret;        newLocation.Timestamp = now;        if (TrailCount != 0) {            newLocation.PreviousLocationId = Trail[TrailCount].LocationId;        }        Trail[TrailCount] = newLocation;        TrailCount++;    }    function GetTrailCount() returns (uint8) {        return TrailCount;    }        function GetLocation(uint8 TrailNo) returns (string,uint,uint,uint,string) {        return(Trail[TrailNo].Name, Trail[TrailNo].LocationId, Trail[TrailNo].PreviousLocationId, Trail[TrailNo].Timestamp, Trail[TrailNo].Secret);    }}';
-      web3.eth.compile.solidity(foodSafeSource, function(error, foodSafeCompiled) {
+      var compiled = web3.eth.compile.solidity(foodSafeSource, function(error, foodSafeCompiled) {
+        console.log(foodSafeCompiled);
+        console.log(error);
         foodSafeABI = foodSafeCompiled['<stdin>:FoodSafe'].info.abiDefinition;
         foodSafeContract = web3.eth.contract(foodSafeABI);
         foodSafeCode = foodSafeCompiled['<stdin>:FoodSafe'].code;
-      })
+      });
+      console.log(compiled);
     });
   },
   createContract: function() {
